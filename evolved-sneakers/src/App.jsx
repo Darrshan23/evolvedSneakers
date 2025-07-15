46c2e14469612af75fe9421bc8c62ec72fdf93de
Darrshan <129675691+Darrshan23@users.noreply.github.com>
2025-07-16 01:34:51 +0800

    Update App.jsx

--- evolved-sneakers/src/App.jsx
+++ evolved-sneakers/src/App.jsx
@@ -5,14 +5,31 @@ const Button = () => {
 }
 
 const Copy = () => {
-  return <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, porro impedit illo in iure repudiandae blanditiis assumenda sapiente sed exercitationem?</p>
+  const copyHandler = () => {
+    console.log("Stopp!!")
+  }
+  
+  return <p onCopy={copyHandler}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, aperiam dicta maiores qui deleniti tenetur similique iste officia reprehenderit. Voluptate?</p>
+}
+
+const Move = () => {
+  function moveHandler () {
+    alert('Move moved event fired');
+    console.log('Move moved event fired')
+  }
+
+  return <p onMouseMove={moveHandler}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente consectetur similique animi quisquam alias assumenda delectus fuga aut esse sint?</p>
 }
 
 const App = () => {
   return (
-    <Button />
+    <section>
+      <Button />
+      <Copy />
+      <Move />
+    </section>
   )
-}
+}1.47.58
 
 export default App
 

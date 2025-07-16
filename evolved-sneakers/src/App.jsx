aef9cfbab715bacf841865c3a72d0eb436d42fb4
Darrshan <129675691+Darrshan23@users.noreply.github.com>
2025-07-17 01:23:08 +0800

    Update App.jsx

--- evolved-sneakers/src/App.jsx
+++ evolved-sneakers/src/App.jsx
@@ -1,35 +1,29 @@
-const Button = () => {
-  const handleClick = () => console.log(Math.round(Math.random()*10))
-
-  return <button onClick={handleClick}>Click Me</button>
-}
-
-const Copy = () => {
-  const copyHandler = () => {
-    console.log("Stopp!!")
-  }
-  
-  return <p onCopy={copyHandler}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, aperiam dicta maiores qui deleniti tenetur similique iste officia reprehenderit. Voluptate?</p>
-}
-
-const Move = () => {
-  function moveHandler () {
-    alert('Move moved event fired');
-    console.log('Move moved event fired')
-  }
-
-  return <p onMouseMove={moveHandler}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente consectetur similique animi quisquam alias assumenda delectus fuga aut esse sint?</p>
-}
+import { useState } from "react"
 
 const App = () => {
-  return (
-    <section>
-      <Button />
-      <Copy />
-      <Move />
-    </section>
+
+  const [movie, setMovie] = useState({
+    title: "Bigil",
+    ratings: "8"
+  })
+
+  const clickHandler = () => {
+    const copyMovie = {
+      ...movie,
+      ratings: 5
+    }
+
+    setMovie(copyMovie)
+  }
+
+
+  return ( 
+    <div>
+      <h1>Title: {movie.title}</h1>
+      <p>Ratings: {movie.ratings}</p>
+      <button onClick={clickHandler}>Update rating</button>
+    </div>
   )
-}1.47.58
+}
 
-export default App
-
+export default App 2:01:56

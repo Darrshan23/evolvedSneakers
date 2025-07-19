91e746040bd0ba835936e4649a4fd3669fd9584b
Darrshan <129675691+Darrshan23@users.noreply.github.com>
2025-07-19 18:36:29 +0800

    Update App.jsx

--- evolved-sneakers/src/App.jsx
+++ evolved-sneakers/src/App.jsx
@@ -1,26 +1,69 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const App = () => {
-  
-  const [name, setName] = useState(["Darr", "King"]);
-  const updatePerson = () => {
-    setName(name.map(n=>(
-      n=== "Darr" ? "Darrrr" : n
-    )))
-  }
+  const [number, setNumber] = useState(1);
+  const [friends, setfriend] = useState(['Darrshan', 'Cy']);
+  const [movie, setMovie] = useState({
+    name: 'Avatar',
+    ratings: 10,
+  });
+  const [movies, setMovies] = useState([
+    { id: 1, title: 'Spider Man', ratings: 8 },
+    { id: 2, title: 'Iron Man 3', ratings: 10 },
+    { id: 3, title: 'The Incredible Hulk', ratings: 7 },
+  ]);
 
-  return(
+  const addOne = () => setNumber(number + 1);
+  const addRaj = () => setfriend([...friends, 'Raj']);
+  const deleteRaj = () =>
+    setfriend(friends.filter((friend) => friend !== 'Raj'));
+  const updateRaj = () => {
+    setfriend(friends.map((friend) => (friend == 'Raj' ? 'Raja' : friend)));
+  };
+  const changeRating = () => {
+    // Longer way
+    // const copyMovie = {
+    //   ...movie,
+    //   ratings: 5,
+    // };
+
+    //setMovie(copyMovie);
+
+    //Shorter way
+    setMovie({ ...movie, ratings: 5 });
+  };
+  const nameChange = () => {
+    setMovies(
+      movies.map((m) => (m.id === 1 ? { ...movies, title: 'John Wick' } : m))
+    );
+  };
+
+  return (
     <div>
-      <h1>
-        <ul>
-          {name.map(n=>(
-            <li key={Math.random()}>{n}</li>
-          ))}
+      <h1 onClick={addOne}>{number}</h1>
+      <hr />
+      <ul>
+        {friends.map((friend) => (
+          <li key={Math.random()}>{friend}</li>
+        ))}
+      </ul>
+      <button onClick={addRaj}>Add Raj</button>
+      <button onClick={deleteRaj}>Delete Raj</button>
+      <button onClick={updateRaj}>Update Raj</button>
+      <hr />
+      <h1>Name: {movie.name}</h1>
+      <p>Ratings: {movie.ratings}</p>
+      <button onClick={changeRating}>Change rating to 5</button>
+      <hr />
+      {movies.map(({ id, title, ratings }) => (
+        <ul key={id}>
+          <li>Title: {title}</li>
+          <li>Ratings: {ratings}</li>
         </ul>
-        <button onClick={updatePerson}>Update Darr</button>
-      </h1>
+      ))}
+      <button onClick={nameChange}>Change Name</button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
